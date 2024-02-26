b15db7bd506e767c5a845cfdb3713fe4faa386e5
Andy Nguyen <andynguyen852@gmail.com>
2024-02-26 10:25:24 -0800

    Add files via upload

--- Counter/Counter/ContentView.swift
+++ Counter/Counter/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @State private var count: Int = 0 // to store count (display below)
     
     var body: some View {
-        VStack {
+        VStack { // to display the prompt, count, and buttons all vertically
             Text("Click buttons below to change the counter") // show prompt
                 .padding() // add empty line
             
@@ -19,38 +19,32 @@ struct ContentView: View {
                 .padding() // add empty line
             
             HStack{ // to have 2 buttons side by side
-                // add 1 to count upon click and shows design of button
-                Button("Increment", action: increment)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
+                Button("Increment", action: increment) // creates button with text "Increment" and calls function increment upon click
+                    .padding() // adds more color from the border to the text
+                    .background(Color.blue) // makes the color blue for button
+                    .foregroundColor(.white) // makes the text white for button
+                    .cornerRadius(8) // makes the button corners round
                 
-                // subtracts 1 to count upon click and shows design of button
-                Button("Increment", action: decrement)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
-            }
-        }
+                Button("Increment", action: decrement) // creates button with text "Decrement" and calls function decrement upon click
+                    .padding() // adds more color from the border to the text
+                    .background(Color.blue) // makes the color blue for button
+                    .foregroundColor(.white) // makes the text white for button
+                    .cornerRadius(8) // makes the button corners round
+            } // to close hstack
+        } // to close vstack
         .padding()
     }
-    // to add 1 to counter
-    func increment(){
-        // only adds 1 if less than 50
-        if (count < 50){
-            count += 1
-        }
-    }
+    func increment(){ // to add 1 to counter
+        if (count < 50){ // only adds 1 if less than 50
+            count += 1 // adds 1 to count
+        } // end of if
+    } // to close increment
     
-    // to subtract 1 to counter
-    func decrement(){
-        // only subtracts 1 if more than 0
-        if (count > 0){
-            count -= 1
-        }
-    }
+    func decrement(){ // to subtract 1 to counter
+        if (count > 0){ // only subtracts 1 if more than 0
+            count -= 1 // subtracts 1 to count
+        } // end of if
+    } // to close decrement
 }
 
 #Preview {
